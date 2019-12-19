d53f3f72f31541ccbdb3a0078cbe15783d227eab
jistjoalal <jistjoalal@gmail.com>
2019-12-18 20:20:35 -0800

    move notes to readme

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Web CPU emulator
 
 Emulating simple CPU's for fun and learning. Inspired by [n2t](https://www.nand2tetris.org/) and [Ben Eater](https://eater.net/).
+
+## Notes / Todo
+
+- permanent ROM storage
+- delete ROM button
+- history display
+- cap history size
+- render speed controls (how many CPU cycles per frame)
+- hex/dec/ascii mode toggle
+- n-bits mode toggle
+- step forward n steps button
--- src/main.js
+++ src/main.js
@@ -3,17 +3,5 @@ import ROMS from "./roms.js";
 import Computer from "./Computer.js";
 
 let root = document.getElementById("root");
-let c = new Computer(10, root);
+let c = new Computer(8, root);
 c.load(ROMS.Counter);
-
-/**
- * TODO:
- * - permanent ROM storage
- * - delete ROM button
- * - history display
- * - cap history size
- * - render speed controls (how many CPU cycles per frame)
- * - hex/dec/ascii mode toggle
- * - n-bits mode toggle
- * - step forward n steps button
- */
